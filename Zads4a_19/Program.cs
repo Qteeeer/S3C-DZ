f1cf276e7edc58e3afaba2fe0b157bf4a13401da
Pavel Filatov <qteeeer@icloud.com>
2022-09-23 23:30:20 +0300

    Готово

---
+++ Zads4a_19/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 19
+// Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+ 
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+int PoliTest(int num)
+{
+    if (num / 10000 == num % 10 && (num / 1000) % 10 == (num / 10) % 10)
+   {
+        Console.WriteLine("Число является палиндромом!!!");
+   }
+   else
+   {
+        Console.WriteLine("Число не является палиндромом!!!");
+   }
+   return num;
+}
+
+int number = ReadData("Введите пятизначное число: ");
+if (number >= 10000 && number <= 99999)
+{
+    PoliTest(number);
+}
+else
+{
+   Console.WriteLine("Вы ввели не пятизначное число!");
+}
+
